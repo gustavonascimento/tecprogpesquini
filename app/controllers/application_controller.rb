2f0337f90d3518580313eb53a5e388872e8f14ed
João Guilherme <joaaogui@gmail.com>
2016-04-06 20:09:48 -0300

    Fixing structure of class in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,17 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::RoutingError, :with => :render_not_found
 
   def raise_not_found!
+
     raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+
   end
 
   def render_not_found
+
     respond_to do |f|
       f.html{ render :template => "errors/404", :status => 404 }
     end
+
   end
+  
 end
