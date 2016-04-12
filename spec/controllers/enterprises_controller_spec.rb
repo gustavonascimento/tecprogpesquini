fa345a4632164bf0653b39db41f6f7ef0cd09233
João Guilherme <joaaogui@gmail.com>
2016-04-11 21:11:57 -0300

    Adding header for enterprise_controller_spec and fixing the structre of the classes

--- spec/controllers/enterprises_controller_spec.rb
+++ spec/controllers/enterprises_controller_spec.rb
@@ -1,25 +1,47 @@
+# File: application_controller_spec.rb
+# Purpouse: Makes a unit test for the enterprise controller
+# License: GPL v3
+# Group 10 Tecprog
+# FGA - Universidade de Brasília - Campus Gama
+
 require 'rails_helper'
 
 RSpec.describe EnterprisesController, :type => :controller do 
+
   before do
+
     @enterprise = Enterprise.create(cnpj: "12345")
+
   end
 
   describe "GET" do 
+
     describe '#index' do
+
       it "should work" do 
+
         get :index
         expect(response).to have_http_status(:success)
+
       end
+
     end
     
     describe '#show' do
+
       describe 'with a registered enterprise' do 
+
         it "should work" do
+
           get :show, :id => @enterprise.id
           expect(response).to have_http_status(:success)
+
         end
+
       end
+
     end
+
   end
+  
 end
