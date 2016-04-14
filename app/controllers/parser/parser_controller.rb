a9b4599e97eba8109da260ee5fb82ea45ea5bc4e
João Guilherme <joaaogui@gmail.com>
2016-04-14 00:43:35 -0300

    Inserting the descriptions for the methods and fixing the structure of classes, of the parser_controller

--- app/controllers/parser/parser_controller.rb
+++ app/controllers/parser/parser_controller.rb
@@ -1,6 +1,14 @@
+# File: parser_controller.rb
+# Purpouse: Does the parser with the file in the csv format.
+# License: GPL v3
+# Group 10 Tecprog
+# FGA - Universidade de Brasília - Campus Gama
+
 class Parser::ParserController < ApplicationController
 
   require 'csv'
+  
+  # stores the .csv file.
   @@filename = 'parser_data/CEIS.csv'
 
   before_filter :authorize, only: [:check_nil_ascii, :check_date, :import, 
@@ -12,23 +20,32 @@ class Parser::ParserController < ApplicationController
 
   end
 
+  # makes sure that there isn't empty ascii characters in the csv file.
   def check_nil_ascii(text)
+
     if text.include?("\u0000")
       return "Não Informado"
     else
       return text.upcase
     end
+
   end
 
+  # checks the date.
   def check_date(text)
+
     begin
       return text.to_date
     rescue
       return nil
     end
+
   end
 
+  # checks and save the data from the file.
   def check_and_save(c)
+
+    # stores all the data from the parser.
     begin
       c.save!
       c
@@ -36,6 +53,7 @@ class Parser::ParserController < ApplicationController
       c = c.refresh!
       c
     end
+
   end
 
 end
