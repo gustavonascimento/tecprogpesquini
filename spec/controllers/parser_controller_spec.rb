4973744b6606d9d24f67135fac31dbe003a74f35
Gustavo Moreira <gustavomn93@gmail.com>
2016-04-16 18:49:15 -0300

    Headers and identation on controllers rspec

--- spec/controllers/parser_controller_spec.rb
+++ spec/controllers/parser_controller_spec.rb
@@ -1,12 +1,26 @@
+# File: application_controller_spec.rb
+# Purpouse: Makes a unit test for the parser controller
+# License: GPL v3
+# Group 10 Tecprog
+# FGA - Universidade de Brasília - Campus Gama
+
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do 
+	
 	describe   "GET" do 
+	
 		describe '#new' do
+	
 			it "should work" do 
+	
 				get :new
 				expect(response).to have_http_status(:success)
+	
 			end
+	
 		end
+	
 	end
+
 end
