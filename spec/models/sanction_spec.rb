d551666481e02a178991750e66a4efb4f9ae5bf5
João Guilherme <joaaogui@gmail.com>
2016-04-17 16:16:58 -0300

    Fixing the identation of the remaining spc models

--- spec/models/sanction_spec.rb
+++ spec/models/sanction_spec.rb
@@ -10,9 +10,11 @@ require 'rails_helper'
 describe Sanction do
 
   before do
+
     @sanction = Sanction.new
     @sanction.process_number = "9090"
     @sanction.save
+
   end
   
   subject { @sanction }
@@ -20,44 +22,65 @@ describe Sanction do
   it { should be_valid }
 
   describe 'percentual_sanction' do
+
     it " to do work" do
+
       b = Sanction.percentual_sanction(10000)
       c = b.to_i
       expect(c).to eq(188)
+
     end
+
   end
 
   describe "uniqueness validation of process_number" do
     
     describe "unique process_number" do
+
       it "should be_valid" do
+
         uniqueness_sanction = Sanction.new
         uniqueness_sanction.process_number = "123" 
         expect(uniqueness_sanction).to be_valid
-          end
-        end
+
+      end
+
+    end
+
     describe "duplicated process_number" do
+
       it "should not be_valid" do
+
         duplicated_sanction = Sanction.new
         duplicated_sanction.process_number = "9090" 
         expect(duplicated_sanction).not_to be_valid
+
       end
+
     end
-  
 
     describe "#refresh!" do
+
       before do
+
         @s = Sanction.new
         @s.process_number = "356754"
         @s.save
+
       end
+
       it "should return sanction" do
+
         expect(@s.refresh!).to eq(@s);
+
       end
 
       it "should not return other sanction" do
+
         expect(@s.refresh!).not_to eq(@sanction);
-      end  
+
+      end
+
     end
 
   end   
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -10,9 +10,11 @@ require 'rails_helper'
 describe State do
 
   before do
+
     @state = State.new
     @state.abbreviation = "DF1"
     @state.save
+
   end
 
   subject {@state}
@@ -25,38 +27,55 @@ describe State do
   describe "uniqueness validation of abbreviation" do
     
     describe "unique abbreviation" do
+
       it "should be_valid" do
+
         uniqueness_state = State.new
         uniqueness_state.abbreviation = "SP1"
         uniqueness_state.save 
         expect(uniqueness_state).to be_valid
+
       end
+
     end
 
    describe "duplicated abbreviation" do
+
         it "should not be_valid" do
+
         duplicated_state = State.new
         duplicated_state.abbreviation = "DF1" 
         expect(duplicated_state).not_to be_valid
+
       end
+
     end
 
 
     describe "#refresh!" do
+
       before do
+
         @s = State.new
         @s.abbreviation = "Es_Teste"
         @s.save
+
       end
 
       it "should return state" do
+
         expect(@s.refresh!).to eq(@s);
+
       end
 
       it "should not return other state" do
+
         expect(@s.refresh!).not_to eq(@state);
+
       end  
+
     end
     
   end
+  
 end
