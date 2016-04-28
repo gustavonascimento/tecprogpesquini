9572800fedd1636c1bf44718e35a366a74b87471
Leandro Alves <leandrosustenido@gmail.com>
2016-04-27 21:36:03 -0300

    restruture js form making it more readeble

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,7 @@
 function main() {
 
   (function () {
-
-     'use strict';
-
-
+   'use strict';
       /*====================================
       Show Menu on Book
       ======================================*/
@@ -13,85 +10,64 @@ function main() {
 
         /*var welcome = document.getElementById('tf-home');*/
         if (document.getElementById('tf-home')  == null) {
-
           $('.navbar-default').addClass('on');
-
         }
-
         else {
-
           $('.navbar-default').removeClass('on');
-        
+
           $(window).bind('scroll', function() {
-
-              var navHeight = $(window).height() - 100;
-
-              if ($(window).scrollTop() > navHeight) {
-
-                  $('.navbar-default').addClass('on');
-
-              } 
-
-              else {
-
-                  $('.navbar-default').removeClass('on');
-
-              }
+            var navHeight = $(window).height() - 100;
+            if ($(window).scrollTop() > navHeight) {
+              $('.navbar-default').addClass('on');
+            } 
+            else {
+              $('.navbar-default').removeClass('on');
+            }
           });
-
-          }
-
+        }
       });
 
       $('body').scrollspy({ 
-
-          target: '.navbar-default',
-          offset: 80
-
+        target: '.navbar-default',
+        offset: 80
       })
 
-    	$(document).ready(function() {
-        
-    	  $("#search").owlCarousel({
-    	 
+      $(document).ready(function() {
+       $("#search").owlCarousel({
     	      navigation : false, // Show next and prev buttons
     	      slideSpeed : 300,
     	      paginationSpeed : 400,
     	      autoHeight : true,
     	      itemsCustom : [
-  				        [0, 1],
-  				        [450, 2],
-  				        [600, 2],
-  				        [700, 2],
-  				        [1000, 4],
-  				        [1200, 4],
-  				        [1400, 4],
-  				        [1600, 4]
-  				      ],
-    	  });
+            [0, 1],
+            [450, 2],
+            [600, 2],
+            [700, 2],
+            [1000, 4],
+            [1200, 4],
+            [1400, 4],
+            [1600, 4]
+            ],
+          });
 
-    	  $("#clients").owlCarousel( {
-    	 
+       $("#clients").owlCarousel( {
     	      navigation : false, // Show next and prev buttons
     	      slideSpeed : 300,
     	      paginationSpeed : 400,
     	      autoHeight : true,
     	      itemsCustom : [
-  				        [0, 1],
-  				        [450, 2],
-  				        [600, 2],
-  				        [700, 2],
-  				        [1000, 4],
-  				        [1200, 5],
-  				        [1400, 5],
-  				        [1600, 5]
-  				      ],
-    	  });
+            [0, 1],
+            [450, 2],
+            [600, 2],
+            [700, 2],
+            [1000, 4],
+            [1200, 5],
+            [1400, 5],
+            [1600, 5]
+            ],
+          });
+     });
+    }());
 
-    	});
-
-
-  }());
-
-  }
-  main();
+}
+main();
