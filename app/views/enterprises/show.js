b10db31a95a43baeb9d21abd36539eccee075c7d
João Guilherme <joaaogui@gmail.com>
2016-06-05 14:47:23 -0300

    Changing and adding headers on enterprises views

--- app/views/enterprises/show.js
+++ app/views/enterprises/show.js
@@ -1,3 +1,11 @@
+/*
+# File: show.js
+# Purpouse: Javascript that loads the tabs of sanctons and payments dynamically.
+# License: GPL v3
+# Group 10 Tecprog
+# FGA - Universidade de Brasília - Campus Gama
+*/
+
 var sanctions_btn = document.getElementById("sanction-info-btn");
 
 if ($(sanctions_btn).hasClass("active"))
