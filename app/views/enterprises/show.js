d4b7594a19ce2bdd813e09c6d0a8143f4eaa4f76
Leandro Alves <leandrosustenido@gmail.com>
2016-06-20 00:00:08 -0300

    reformating view presentation

--- app/views/enterprises/show.js
+++ app/views/enterprises/show.js
@@ -10,9 +10,9 @@ var sanctions_btn = document.getElementById("sanction-info-btn");
 
 if ($(sanctions_btn).hasClass("active"))
 {
-  $(".sanction-info").html("<%= escape_javascript(render("sanctions")) %>");
+	$(".sanction-info").html("<%= escape_javascript(render("sanctions")) %>");
 }
 else
 {
-  $(".payment-info").html("<%= escape_javascript(render("payments")) %>");
+	$(".payment-info").html("<%= escape_javascript(render("payments")) %>");
 }
